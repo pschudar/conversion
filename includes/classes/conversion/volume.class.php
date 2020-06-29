9ee714a3639b1dd3599284e12230e3732c0e4210
Paul Schudar <39098048+pschudar@users.noreply.github.com>
2020-06-29 07:50:15 -0600

    Update volume.class.php

--- includes/classes/conversion/volume.class.php
+++ includes/classes/conversion/volume.class.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace conversion;
 
+/**
+ * Volume
+ * 
+ * @category volume
+ * @package conversion
+ * @author Paul Schudar
+ * @copyright Copyright (c) 2020, Paul Schudar
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ * @internal Common Unit: Liters
+ */
 class Volume {
 
     use \calc\Calculate;
@@ -34,13 +44,8 @@ class Volume {
         'milliliters' => 0.001
     ];
 
-    private $value;
-    private $processedValue;
-
     /**
-     * Processes the volume and capacity conversions
-     * 
-     * @internal Common Unit: Liters
+     * Processes the conversion
      * 
      * @param string $value
      * @param string $from_unit
@@ -48,9 +53,7 @@ class Volume {
      * @return float
      */
     public function processConversion(float $value, string $from_unit, string $to_unit) {
-        $this->value = self::convertToUnit($value, $from_unit, $to_unit);
-        $this->processedValue = self::convertFromUnit($this->value, $to_unit, $from_unit);
-        return $this->processedValue;
+        return self::processConversions($value, $from_unit, $to_unit);
     }
 
 }
